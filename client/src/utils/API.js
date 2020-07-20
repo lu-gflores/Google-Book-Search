4f6143d33eb68c53e1b04725a11b39500f3c97e1
George Flores <georgeflores886@gmail.com>
2020-07-20 17:26:11 -0400

    updated api.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,12 +2,8 @@ import axios from 'axios'
 
 export default {
     // Gets all books
-    getBooks: function() {
-      return axios.get("/api/books");
-    },
-    // Gets the book with the given id
-    getBook: function(id) {
-      return axios.get("/api/books/" + id);
+    getBooks: function(query) {
+      return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
     },
     // Deletes the book with the given id
     deleteBook: function(id) {
